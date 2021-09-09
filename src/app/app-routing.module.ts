c0a7817a87a8171f56e96a3e637f27441266e67f
Pertoldi <antoine.pertoldi@gmail.com>
2021-09-09 18:29:36 +0200

    footer OP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 import { AccueilComponent } from './page/accueil/accueil.component';
 
 const routes: Routes = [
   { path: '', component: AccueilComponent },
-
+  { path: 'login', component: LoginComponent},
+  { path: '**', redirectTo: ''}
 ];
 
 @NgModule({
