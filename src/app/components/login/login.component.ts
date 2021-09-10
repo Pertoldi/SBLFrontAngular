f27b5039292d3209372fe90f66683d057068b83d
Pertoldi <antoine.pertoldi@gmail.com>
2021-09-10 11:23:32 +0200

    login form OP, userservice initilize

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+	loginForm!: FormGroup;
 
-  ngOnInit(): void {
-  }
+	constructor(private formBuilder: FormBuilder) { }
 
+	ngOnInit(): void {
+		this.initLoginForm();
+	}
+
+	initLoginForm() {
+		this.loginForm = this.formBuilder.group({
+			email: ['', [Validators.required, Validators.email]],
+			password: ['', [Validators.required]]
+		})
+	}
+
+	onSubmitLoginForm() {
+		console.log(this.loginForm);
+		//TODO post request with user Service Add to this service a subject to keep traking when the user is co or not
+	}
 }
