a6f2b1247b80aba34039d807d4506269451fdd93
Pertoldi <antoine.pertoldi@gmail.com>
2024-09-29 17:35:12 +0200

    MAJ 29/09/2024

--- src/app/components/school/school.component.ts
+++ src/app/components/school/school.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SchoolComponent implements OnInit {
 
-  constructor() { }
+  // Array of image paths
+  imageArray: string[] = [];
+
+  constructor() {
+    this.loadImages();
+  }
 
   ngOnInit(): void {
   }
 
+  // Method to dynamically load image paths 
+  loadImages() {
+    for (let i = 1; i <= 10; i++) {
+      this.imageArray.push(`assets/ecole/2024/Association SKATEBOARD BROTHERHOOD Lozère (${i}).JPG`);
+    }
+    console.log('imageArray is :', this.imageArray)
+  }
 }
